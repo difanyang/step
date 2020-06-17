2efe7bd1c3c27965f3b678cc7c3d44a4c1284e6c
difanyang <1006380730@qq.com>
2020-06-17 00:19:18 +0000

    Fetch comments data with JSON formatting

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,6 +15,8 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,10 +25,18 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+  static ArrayList<String> stringList = new ArrayList<String>();
+  static {
+    stringList.add("Nice to meet you!");
+    stringList.add("Have a nice day!");
+    stringList.add("Stay safe and healthy!");
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Difan Yang!");
+    Gson gson = new Gson();
+    String json = gson.toJson(stringList);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -27,8 +27,19 @@ function addRandomFacts() {
   factContainer.innerText = fact;
 }
 
-/** Fetches the content in DataServlet and display on Home page */
+/** Fetches comments from the servers and adds them to the DOM. */
 function getData() {
-  fetch('/data').then(response => response.text()).then((data) => 
-  {document.getElementById("data-container").innerText = data;});
+  fetch('/data').then(response => response.json()).then((data) => 
+  {
+    const arrayListElement = document.getElementById('data-container');
+    arrayListElement.innerHTML = '';
+    arrayListElement.appendChild(createListElement(data));
+  });
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
 }
